090e0bddf129e57f4797c8f40e29af0e1d810168
Yurii Piets <yurii.piets@gmail.com>
2018-03-12 08:50:04 +0100

    impleented lab1

---
+++ lab1/src/main/java/com/soa/TextServlet.java
@@ -0,0 +1,23 @@
+package com.soa;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TextServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Witaj, JBoss AS 7");
+        out.close();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
