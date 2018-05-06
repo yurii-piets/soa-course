79e0f15846c8de224a4b18ef3804468e834f114d
Yurii Piets <yurii.piets@gmail.com>
2018-05-06 16:06:19 +0200

    Lab 5 | zad 3 notification consumer

---
+++ lab6/library/library-web/src/main/java/com/soa/jms/NotificationMDB.java
@@ -0,0 +1,25 @@
+package com.soa.jms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(name = "Receiver", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jms/LibraryQueue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
+})
+public class NotificationMDB implements MessageListener {
+
+    public void onMessage(Message message) {
+        TextMessage tm = (TextMessage) message;
+        try {
+            System.out.println(tm.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
