23bdca5aa822101cad489e10ad315b924751cf08
Yurii Piets <yurii.piets@gmail.com>
2018-05-07 09:27:25 +0200

    Lab 8 | separated persistence unit

--- lab8/src/main/java/com/soa/Application.java
+++ lab8/src/main/java/com/soa/Application.java
@@ -1,24 +1,8 @@
 package com.soa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Application {
 
-    public void run() {
-        Map<String, String> env = System.getenv();
-        Map<String, Object> config = new HashMap<>();
-        config.put("javax.persistence.jdbc.user", env.get("PG_USER"));
-        config.put("javax.persistence.jdbc.password", env.get("PG_PASSWORD"));
-
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("SOA_LAB8", config);
-        EntityManager em = factory.createEntityManager();
-    }
-
     public static void main(String[] args) {
-        new Application().run();
+        new PersistenceManager();
     }
 }
